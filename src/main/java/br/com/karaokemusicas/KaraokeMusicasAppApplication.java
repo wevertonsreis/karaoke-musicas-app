711b858bbc19f4b247ffc09c2b0fa04d1216f1da
Weverton Reis <weverton.sant.reis@gmail.com>
2020-02-26 13:22:26 -0300

    primeiro commit

---
+++ src/main/java/br/com/karaokemusicas/KaraokeMusicasAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.karaokemusicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaraokeMusicasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaraokeMusicasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/karaokemusicas/KaraokeMusicasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.karaokemusicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaraokeMusicasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
